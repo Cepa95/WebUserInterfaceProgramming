3df0648879b2a3ada2395a3fd54a62e343e2868e
Cepa95 <ceprnic.josip@gmail.com>
2023-03-02 00:17:06 +0100

    Update prvi.js

--- js/prvi.js
+++ js/prvi.js
@@ -1,5 +1,20 @@
 function greet(){
-    console.log('Hello World')
+    console.log('Hello World!')
 }
 
+function isPrime(){
+
+const number = parseInt(prompt('Unesite neki broj, za koji zelite provjerit jeli prost: '));
+if (Number.isInteger(number)){
+    for (let i = 2; i < number; i++){
+         if (number % i == 0)
+            return false;
+    }
+    return true;
+}
+return null;
+}
+
+
 greet();
+console.log(isPrime());
