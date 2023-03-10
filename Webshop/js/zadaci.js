feeba05f0f4ed1346fd65d7393bb4f4934035823
Cepa95 <ceprnic.josip@gmail.com>
2023-03-11 00:30:27 +0100

    first two finished

---
+++ Webshop/js/zadaci.js
@@ -0,0 +1,44 @@
+// alert("Moj prvi JS program");
+// console.log("Moj prvi JS program");
+
+// function arithmetic_middle() {
+//   const first = parseInt(prompt("Unesite prvi broj: "));
+//   const second = parseInt(prompt("Unesite drugi broj: "));
+//   const third = parseInt(prompt("Unesite treci broj: "));
+
+//   if (
+//     !Number.isInteger(first) ||
+//     !Number.isInteger(second) ||
+//     !Number.isInteger(third)
+//   ) {
+//     console.log("Krivo unešeni podaci");
+//     return;
+//   }
+//   const average = (first + second + third) / 3;
+//   console.log("Aritmetička sredina:" + average);
+// }
+
+// function math_operations(first, second, operation) {
+//   let solution;
+//   let check = "+-*/";
+
+//   if (isNaN(first) || isNaN(second) || !check.includes(operation)) {
+//     console.log("Pogrešan unos broja ili operacije!");
+//     return;
+//   } else if (operation === "+") {
+//     solution = first + second;
+//   } else if (operation === "-") {
+//     solution = first - second;
+//   } else if (operation === "*") {
+//     solution = first * second;
+//   } else {
+//     solution = first / second;
+//   }
+//   console.log(solution);
+// }
+
+// //arithmetic_middle();
+// const first = parseFloat(prompt("Unesite prvi broj: "));
+// const second = parseFloat(prompt("Unesite drugi broj: "));
+// let operation = prompt("Unesite operaciju: ");
+// math_operations(first, second, operation);
