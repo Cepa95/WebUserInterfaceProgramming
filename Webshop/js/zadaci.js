dd76bef2ed80f0d833b4264b311b2c2120f00453
Cepa95 <ceprnic.josip@gmail.com>
2023-03-11 01:30:19 +0100

    Update zadaci.js

--- Webshop/js/zadaci.js
+++ Webshop/js/zadaci.js
@@ -1,11 +1,12 @@
 // alert("Moj prvi JS program");
 // console.log("Moj prvi JS program");
 
+
+//1
 // function arithmetic_middle() {
 //   const first = parseInt(prompt("Unesite prvi broj: "));
 //   const second = parseInt(prompt("Unesite drugi broj: "));
 //   const third = parseInt(prompt("Unesite treci broj: "));
-
 //   if (
 //     !Number.isInteger(first) ||
 //     !Number.isInteger(second) ||
@@ -18,10 +19,10 @@
 //   console.log("Aritmetička sredina:" + average);
 // }
 
+//2
 // function math_operations(first, second, operation) {
 //   let solution;
 //   let check = "+-*/";
-
 //   if (isNaN(first) || isNaN(second) || !check.includes(operation)) {
 //     console.log("Pogrešan unos broja ili operacije!");
 //     return;
@@ -37,6 +38,7 @@
 //   console.log(solution);
 // }
 
+//3
 // function first_digit(first) {
 //   while (first >= 10) {
 //     first /= 10;
@@ -44,6 +46,7 @@
 //   return parseInt(first);
 // }
 
+//4
 // function season(month) {
 //   if (!Number.isInteger(month)) {
 //     alert("nije unesen integer");
@@ -60,6 +63,21 @@
 //   }
 // }
 
+//5
+// function count_Z(number, Z) {
+//     let counter = 0;
+//     let rest;
+//     while (number != 0) {
+//       rest = number % 10;
+//       if (rest === Z) {
+//         counter += 1;
+//       }
+//       number = parseInt(number / 10);
+//     }
+//     return counter;
+//   }
+  
+
 //1
 // //arithmetic_middle();
 //2
@@ -73,3 +91,14 @@
 //4
 // const month = parseInt(prompt("Unesite mjesec putem brojevnog zapisa"));
 // season(month);
+//5
+// let number = 2955;
+// let Z = 5;
+// alert(
+//   "U broju " +
+//     number +
+//     " imamo " +
+//     count_Z(number, Z) +
+//     " pojavljivanja broja" +
+//     Z
+// );
