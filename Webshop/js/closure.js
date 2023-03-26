50b005dee50237fdbabd6fcc79ffd2bc7db6f237
Cepa95 <ceprnic.josip@gmail.com>
2023-03-27 00:37:57 +0200

    Update closure.js

--- Webshop/js/closure.js
+++ Webshop/js/closure.js
@@ -61,25 +61,43 @@
 //     // ili
 //     console.log(sum(10)(3,12));
 
+// // zadatak5
+// const multiplication = (num) => {
+//     const pseudoRecursive = (number) => {
+//         if ( number > 0 && number < 20) {
+//             num *= number;
+//             return pseudoRecursive; // nemaju () jer je to vise referenca na sebe u ovom slucaju, nego poziv => currying
+//         }
+//         if (number != undefined)
+//         {
+//             return pseudoRecursive; // u slucaju, da ima sta u zagradi, ali broj nije unutar trazenog
+//         }
+//         else { 
+//             return num; // zavrsi rekurziju
+//         }
+//     }
+//     return pseudoRecursive; // u slucaju da neman num, da mi vrati undefined umjesto errora
+// }
+
+// const sum = multiplication(1)(10)(2)(5)(100)(2)(); // prva zagrada mora biti jedan, a zadnja prazna => currying
+// console.log(`Umnozak pozitivnih brojeva manjih od 20 iznosi: ${sum}`)
+
+
 // zadatak5
-const multiplication = (num) => {
-    const pseudoRecursive = (number) => {
-        if ( number > 0 && number < 20) {
-            num *= number;
-            return pseudoRecursive; // nemaju () jer je to vise referenca na sebe u ovom slucaju, nego poziv => currying
-        }
-        if (number != undefined)
-        {
-            return pseudoRecursive; // u slucaju, da ima sta u zagradi, ali broj nije unutar trazenog
-        }
-        else { 
-            return num; // zavrsi rekurziju
-        }
-    }
-    return pseudoRecursive; // u slucaju da neman num, da mi vrati undefined umjesto errora
-}
+// const multiplication = (num) => {
+//     const pseudoRecursive = (number) => {
+//         if ( number > 0 && number < 20) {
+//             num *= number;
+//             return pseudoRecursive; 
+//         }
+//         else if (number == undefined)
+//         {
+//             return num; 
+//         }
+//         return pseudoRecursive;
+//     }
+//     return pseudoRecursive; 
+// }
 
-const sum = multiplication(1)(10)(2)(5)(100)(2)(); // prva zagrada mora biti jedan, a zadnja prazna => currying
-console.log(`Umnozak pozitivnih brojeva manjih od 20 iznosi: ${sum}`)
-
-
+// const sum = multiplication(1)(10)(2)(5)(100)(2)(); 
+// console.log(`Umnozak pozitivnih brojeva manjih od 20 iznosi: ${sum}`)
