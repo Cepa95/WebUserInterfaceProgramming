da9967952abfcdb67b0ef42c8351ebcafecc441e
Cepa95 <ceprnic.josip@gmail.com>
2023-04-02 23:19:52 +0200

    object.js

---
+++ Webshop/js/object.js
@@ -0,0 +1,22 @@
+// alert("Provjera");
+
+// // zadatak1
+// function ocjena(k1, k2, sem) {
+//   //𝑂𝑐𝑗𝑒𝑛𝑎(%) = 0,42*kol1 + 0,42*kol2 + 0,16*semi
+//   return k1 * 0.42 + k2 * 0.42 + sem * 0.16;
+// }
+// console.log(`Postotak ostvaren na kolegiju ${ocjena(50, 80, 80)}`);
+
+// const grade = (k1, k2, sem) => k1 * 0.42 + k2 * 0.42 + sem * 0.16; // bez {} mogu bez returna
+// console.log(`Postotak ostvaren na kolegiju => ${grade(50, 80, 80)}`);
+
+// function gradeCurry(k1) {
+//   return function (k2) {
+//     return function (sem) {
+//       return k1 * 0.42 + k2 * 0.42 + sem * 0.16;
+//     };
+//   };
+// }
+// console.log(`Postotak ostvaren na kolegiju => ${gradeCurry(50)(80)(80)}`);
+
+
