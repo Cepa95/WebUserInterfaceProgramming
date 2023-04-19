213fd02cbfabb209342cf59184c31bc817464049
Cepa95 <ceprnic.josip@gmail.com>
2023-04-20 00:26:19 +0200

    Update proba2.js

--- Webshop/test/proba2.js
+++ Webshop/test/proba2.js
@@ -11,3 +11,21 @@
 // console.log(add(5));
 
 // 3 zadatak
+// function sum(x) {
+//   if (x < 10) {
+//     x = 0;
+//   }
+//   return function (y) {
+//     if (y === undefined) {
+//       return x;
+//     }
+//     if (y > 10) {
+//       return sum(x + y);
+//     } 
+//     else {
+//       return sum(x);
+//     }
+//   };
+// }
+// console.log(sum(1)(2)(20)(35)(4)(7)())
+// console.log(sum(1)(2)(2)(3)(4)(7)())
