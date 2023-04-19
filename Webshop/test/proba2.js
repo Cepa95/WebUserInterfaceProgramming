0627c608886e4fe62b4c584c75b3ef88d77812d0
Cepa95 <ceprnic.josip@gmail.com>
2023-04-20 00:22:29 +0200

    test3

---
+++ Webshop/test/proba2.js
@@ -0,0 +1,13 @@
+// alert("provjera")
+
+// 2 zadatak
+
+// function addN(number) {
+//   return function (a) {
+//     return number + a;
+//   };
+// }
+// const add = addN(3);
+// console.log(add(5));
+
+// 3 zadatak
