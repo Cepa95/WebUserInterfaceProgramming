e8e698da9c5cc56fa1479fe860432a8994115cde
Cepa95 <ceprnic.josip@gmail.com>
2023-05-02 00:13:48 +0200

    Update inheritance.js

--- Webshop/js/inheritance.js
+++ Webshop/js/inheritance.js
@@ -25,3 +25,49 @@
 // console.log(mojAutomobil);
 // mojAutomobil.dodajBrzinu(20);
 // mojAutomobil.prikaziBrzinu();
+
+//zadatak2
+// function Vechicle(brand, year) {
+//   this.brand = brand;
+//   this.year = year;
+//   this.drive = function () {
+//     console.log(`Vozim ${this.brand} vozilo godiste ${this.year}`);
+//   };
+// }
+// // const vehicle = new Vechicle("Toyota", 2000)
+// // vehicle.drive()
+
+// function Auto(brand, year, color, door) {
+//   Vechicle.call(this, brand, year);
+//   this.color = color;
+//   this.door = door;
+//   this.park = function () {
+//     console.log(`Automobil ${this.brand}, ${this.color} boje je parkiran`);
+//   };
+// }
+
+// Auto.prototype = Object.create(Vechicle.prototype);
+// Auto.prototype.constructor = Auto;
+
+// // const automobil = new Auto("Fiat", "Punto", "plave", 1995);
+// // automobil.park();
+
+// function Motorcycle(brand, year, type) {
+//   Auto.call(this, brand, year);
+//   this.type = type;
+//   this.driveFast = function () {
+//     console.log(
+//       `Vozim motocikl tipa ${this.type}, marked ${this.brand} i godiste ${this.year} brzo!`
+//     );
+//   };
+// }
+// Motorcycle.prototype = Object.create(Auto.prototype);
+// Motorcycle.prototype.constructor = Motorcycle;
+
+// let auto = new Auto("BMW", 2022, "crna", 5);
+// auto.drive(); // Vozim marku vozila BMW godište 2022
+// auto.park(); // Automobil BMW, crna boje je parkiran!
+// // Kreiranje instance objekta Motocikl
+// let moto = new Motorcycle("Honda", 2020, "sportski");
+// moto.drive(); // Vozim marku vozila Honda godište 2020
+// moto.driveFast(); // Vozim motocikl tipa sportski, marke Honda i godište 2020 brzo!
