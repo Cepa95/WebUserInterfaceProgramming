b1d3ac46908dce8baf29eee33b7fa903e4563009
Cepa95 <ceprnic.josip@gmail.com>
2023-05-01 23:45:02 +0200

    inheritance

---
+++ Webshop/js/inheritance.js
@@ -0,0 +1,27 @@
+// alert("Provjera");
+
+// zadatak1
+// function Automobil(brand, model, year, speed) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.year = year;
+//   this.speed = speed;
+// }
+// // Automobil.prototype.dodajBrzinu = function (speed) {
+// //   this.speed += speed;
+// // };
+// // Automobil.prototype.prikaziBrzinu = function () {
+// //   console.log(this.speed);
+// // };
+// const mojAutomobil = new Automobil("Toyota", "Cambri", 2015, 60);
+// console.log(mojAutomobil);
+
+// Automobil.prototype.dodajBrzinu = function (speed) {
+//   this.speed += speed;
+// };
+// Automobil.prototype.prikaziBrzinu = function () {
+//   console.log(`Trenutna brzina je ${this.speed} km/h`);
+// };
+// console.log(mojAutomobil);
+// mojAutomobil.dodajBrzinu(20);
+// mojAutomobil.prikaziBrzinu();
