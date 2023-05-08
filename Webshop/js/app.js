b36b04621620bb0762aa4361bdf935bb89328d0f
Cepa95 <ceprnic.josip@gmail.com>
2023-05-08 23:56:07 +0200

    dom

--- Webshop/js/app.js
+++ Webshop/js/app.js
@@ -4,22 +4,28 @@
 // const prednosti = document.getElementById("prednosti");
 // prednosti.style.backgroundColor = "yellow";
 
-
 // const prednosti = document.getElementById("prednosti").style.backgroundColor = "yellow";
 
-
+// querySelectorAll()
 // // zadatak2
 // const odjeljak = document.getElementsByClassName("odjeljak")
 // for (let i = 0; i < odjeljak.length; i++) {
 
-//     odjeljak[i].style.fontSize = '16px'; 
+//     odjeljak[i].style.fontSize = '16px';
 // }
-
 
 // // zadatak3
 // document.getElementById("prednosti").innerHTML = "Ova sekcija je promijenjena koristenjem DOM-a"
 
-
 // prednosti.innerHTML = "Ova sekcija je promijenjena koristenjem DOM-a"
 
-// zadatak4
+// // zadatak4
+// const section = document.getElementsByTagName("section");
+// for (let i = 0; i < section.length; i++) {
+//   section[i].style.backgroundColor = "blue";
+// }
+
+// // zadatak5
+function changeBackgroundColor(){
+    
+}
