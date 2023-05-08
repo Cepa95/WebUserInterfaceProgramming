69e70153c7257e4d20c1a5ad25d2e757f7114504
Cepa95 <ceprnic.josip@gmail.com>
2023-05-08 23:44:38 +0200

    dom

---
+++ Webshop/js/app.js
@@ -0,0 +1,25 @@
+// alert("Provjera");
+
+// // zadatak1
+// const prednosti = document.getElementById("prednosti");
+// prednosti.style.backgroundColor = "yellow";
+
+
+// const prednosti = document.getElementById("prednosti").style.backgroundColor = "yellow";
+
+
+// // zadatak2
+// const odjeljak = document.getElementsByClassName("odjeljak")
+// for (let i = 0; i < odjeljak.length; i++) {
+
+//     odjeljak[i].style.fontSize = '16px'; 
+// }
+
+
+// // zadatak3
+// document.getElementById("prednosti").innerHTML = "Ova sekcija je promijenjena koristenjem DOM-a"
+
+
+// prednosti.innerHTML = "Ova sekcija je promijenjena koristenjem DOM-a"
+
+// zadatak4
