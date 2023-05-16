0de90c5222f57dc8dc833872223835c966814fe6
Cepa95 <ceprnic.josip@gmail.com>
2023-05-16 13:47:06 +0200

    Update async.js

--- Webshop/js/async.js
+++ Webshop/js/async.js
@@ -1,24 +1,43 @@
 // alert("Provjera");
 // // zadatak1
 // function delayedGreeting(greeting) {
-//     return new Promise((resolve, reject) => {
-//       if (typeof greeting === "string") {
-//         setTimeout(() => {
-//           resolve(console.log(greeting));
-//         }, 5000);
-//       } 
-//       else {
-//         reject(new Error("Not a string!"));
-//       }
-//     });
-//   }
+//   return new Promise((resolve, reject) => {
+//     if (typeof greeting === "string") {
+//       setTimeout(() => {
+//         resolve(console.log(greeting));
+//       }, 5000);
+//     } else {
+//       reject(new Error("Not a string!"));
+//     }
+//   });
+// }
 
-//   delayedGreeting('Pozdrav!')
-//     .then(() => {
-//       console.log('Pozdrav je isprintan nakon kašnjenja.');
-//     })
-//     .catch((error) => {
-//         console.error(`Došlo je do pogreške: ${error}`);
-//     });
-  
-  
+// delayedGreeting("Pozdrav!")
+//   .then(() => {
+//     console.log("Pozdrav je isprintan nakon kasnjenja.");
+//   })
+//   .catch((error) => {
+//     console.error(`Doslo je do greske: ${error}`);
+//   });
+
+// // zadatak2
+// function checkWeather(city) {
+//   return new Promise((resolve, reject) => {
+//     const check = Math.random() * 100;
+//     setTimeout(() => {
+//       if (check > 50) {
+//         resolve(`Vrijeme je sunčano u ${city}.`);
+//       } else {
+//         reject(`Vrijeme nije sunčano u ${city}.`);
+//       }
+//     }, 2000);
+//   });
+// }
+
+// checkWeather("Split")
+//   .then((message) => {
+//     console.log(message);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
