bebe873a4b59d04921618ad9829ede7299a22fb0
Cepa95 <ceprnic.josip@gmail.com>
2023-05-16 13:33:06 +0200

    async

---
+++ Webshop/js/async.js
@@ -0,0 +1,24 @@
+// alert("Provjera");
+// // zadatak1
+// function delayedGreeting(greeting) {
+//     return new Promise((resolve, reject) => {
+//       if (typeof greeting === "string") {
+//         setTimeout(() => {
+//           resolve(console.log(greeting));
+//         }, 5000);
+//       } 
+//       else {
+//         reject(new Error("Not a string!"));
+//       }
+//     });
+//   }
+
+//   delayedGreeting('Pozdrav!')
+//     .then(() => {
+//       console.log('Pozdrav je isprintan nakon kašnjenja.');
+//     })
+//     .catch((error) => {
+//         console.error(`Došlo je do pogreške: ${error}`);
+//     });
+  
+  
