d94bb487f99c8d1e2ead9713e14572a6f1659988
Cepa95 <ceprnic.josip@gmail.com>
2023-05-24 23:23:59 +0200

    Update script.js

--- Webshop/test/drugi/script.js
+++ Webshop/test/drugi/script.js
@@ -5,23 +5,22 @@
 //     this.iznos = iznos
 // }
 // Bank.prototype.getBalance = function(){
-//     return this.iznos 
+//     return this.iznos
 // }
 // Bank.prototype.deposit = function(uplata){
-//     return this.iznos += uplata 
+//     return this.iznos += uplata
 // }
 
 // Bank.prototype.withdraw = function(isplata){
 //     if(this.iznos >= isplata)
 //         this.iznos -= isplata
 //     else
-//         console.log("insufficient amount on the account")  
+//         console.log("insufficient amount on the account")
 // }
 
 // const bank = new Bank(500)
 // console.log(`Stanje racuna je ${bank.getBalance()}`)
 // console.log(`Stanje racuna nakon uplate je ${bank.deposit(200)}`)
-
 
 // function Interest(iznos, kamate){
 //     Bank.call(this, iznos)
@@ -36,5 +35,49 @@
 // }
 
 // const k = new Interest(100, 2.5)
-// console.log("Stanje racuna je " + k.getBalance())
-// console.log("Stanje s kamatom je " + k.getInterest(2.5))
+// console.log(`Stanje računa je ${k.getBalance()}`);
+// console.log(`Stanje s kamatom je ${k.getInterest(2.5)}`);
+
+// S klasama
+// class Bank {
+//   constructor(iznos) {
+//     this.iznos = iznos;
+//   }
+// }
+
+// Bank.prototype.getBalance = function () {
+//   return this.iznos;
+// };
+
+// Bank.prototype.deposit = function (uplata) {
+//   return (this.iznos += uplata);
+// };
+
+// Bank.prototype.withdraw = function (isplata) {
+//   if (this.iznos >= isplata) {
+//     this.iznos -= isplata;
+//   } else {
+//     console.log("Nedovoljan iznos na računu");
+//   }
+// };
+
+// class Interest extends Bank {
+//   constructor(iznos, kamate) {
+//     super(iznos);
+//     this.kamate = kamate;
+//   }
+// }
+
+// Interest.prototype.getInterest = function (kamata) {
+//   this.iznos += this.iznos * (kamata / 100);
+//   return this.iznos;
+// };
+
+// const bank = new Bank(500);
+// console.log(`Stanje računa je ${bank.getBalance()}`);
+// console.log(`Stanje računa nakon uplate je ${bank.deposit(200)}`);
+// console.log(`Stanje računa nakon isplate je ${bank.withdraw(100)}`);
+
+// const k = new Interest(100, 2.5);
+// console.log(`Stanje računa je ${k.getBalance()}`);
+// console.log(`Stanje s kamatom je ${k.getInterest(2.5)}`);
