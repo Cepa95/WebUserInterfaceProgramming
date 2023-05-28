fec0d309485c42b2bb527c40a2abd69d76e6a760
Cepa95 <ceprnic.josip@gmail.com>
2023-05-29 00:08:18 +0200

    Update script.js

--- Webshop/test/drugi/script.js
+++ Webshop/test/drugi/script.js
@@ -111,8 +111,6 @@
 //     console.log(error);
 //   });
 
-
-
 // function trianglePerimeter(first, second, third) {
 //   return new Promise(function (resolve, reject) {
 //     if (typeof first === "number" && typeof second === "number" && typeof third === "number") {
@@ -125,3 +123,83 @@
 // }
 
 // trianglePerimeter(12, 3, "b").then(console.log).catch(console.log);
+
+// function Building(name) {
+//   this.name = name;
+// }
+
+// Building.prototype.numberOfApartments = function (number) {
+//   console.log(`In the building ${this.name} there is ${number} apartments`);
+// };
+
+// function Parking(name, spaces) {
+//   Building.call(this, name);
+//   this.spaces = spaces;
+// }
+
+// Parking.prototype = Object.create(Building.prototype);
+// Parking.prototype.constructor = Building;
+
+// Parking.prototype.numberOfSpaces = function (number) {
+//   console.log(`there is ${number} ${this.spaces} parking spaces`);
+// };
+
+// const building = new Parking("Zgrada", "big");
+// console.log(building.name);
+// console.log(building.spaces);
+// building.numberOfApartments(10);
+// building.numberOfSpaces(20);
+
+
+
+// function checkNumber(number) {
+
+//   return new Promise((resolve,reject)=> {
+//     if (number % 2 == 0)
+//     {
+//         resolve(3);
+//     }
+//     else{
+//         reject(4);
+//     }
+//   })
+// }
+
+// checkNumber(11).then(console.log).catch(console.log);
+
+
+// function multiply(a,b, callback){
+
+//     return callback(a,b)
+// }
+
+// function sum(a,b){
+
+//     return a*b;
+// }
+
+// console.log(multiply(10,3,sum))
+
+
+// function waitTime() {
+//     return new Promise((resolve) => {
+//       setTimeout(function() {
+//         console.log("Time");
+//         resolve();
+//       }, 5000);
+//     });
+//   }
+  
+  
+// async function checkNumber(number) {
+
+//     console.log(number)
+//     await waitTime();
+//     console.log('done')
+// }
+
+// checkNumber(10)
+
+
+
+
